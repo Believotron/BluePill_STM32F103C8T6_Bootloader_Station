0f9a7dbc1ee9f6e4c25f24ae586c1736bb967fd6
Daniel D Lindmark <daniel.dee@gmail.com>
2018-05-21 11:56:39 -0500

    Deprecated; Old way to load firmware

---
+++ Arduino/SelfTest/SelfTest.ino
@@ -0,0 +1,14 @@
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);           // set pin to input
+
+
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);       // turn on pullup resistors
+
+}
---
+++ Bootload_Generic.py
@@ -0,0 +1,1 @@
+sudo python ./Python_Bootloader/stm32loader.py -p /dev/ttyS75 -w ~/binaries/Blue_Pill_Generic/generic_boot20_pc13.bin
